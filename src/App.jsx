9b5b385a965d438c8b6c69daba81a416bf1d9f24
Ivan Sanchez <isanchfo@emeal.nttdata.com>
2026-03-17 12:18:27 -0500

    Fix: Add missing testType and handleStartTest to App.jsx destructuring

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,7 @@ function App() {
         handleLogin, handleCreateGroup, handleAnswer, 
         handleToggleGroupTest, handleToggleGroupSurvey,
         handleAddMember, handleDeleteMember, handleSurveySubmit,
+        handleStartTest, testType,
         fetchGroupDetails, groupMembers, groupAnalyticsFilter, setGroupAnalyticsFilter
     } = useAppLogic();
 
