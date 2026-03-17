651a7d9ed143d795110ee6ac1d47fb27d4dfb941
Ivan Sanchez <isanchfo@emeal.nttdata.com>
2026-03-17 15:55:26 -0500

    Force redeploy v1.6: Final stabilization and cache burst

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// Force redeploy v1.2
+// Force redeploy v1.6 - Definitiva
 import { useAppLogic } from './hooks/useAppLogic';
 import Header from './components/Header';
 import LoginForm from './components/LoginForm';
